8dd1630c70bdacb351857f91638477b03cc2aac0
Jack <Jack@lemonlime.tech>
2023-07-05 16:42:45 -0700

    fix placement rules of the pile o' bones

--- src/main/java/kelvin/slendermod/block/BonesBlock.java
+++ src/main/java/kelvin/slendermod/block/BonesBlock.java
@@ -1,15 +1,14 @@
 package kelvin.slendermod.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFacingBlock;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
 
 public class BonesBlock extends HorizontalFacingBlock {
 
@@ -40,5 +39,15 @@ public class BonesBlock extends HorizontalFacingBlock {
 
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+    }
+
+    @Override
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        return world.getBlockState(pos.down()).isSideSolidFullSquare(world,pos,Direction.UP);
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        return this.canPlaceAt(state,world,pos) ? state :  Blocks.AIR.getDefaultState();
     }
 }
